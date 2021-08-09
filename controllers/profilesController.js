4941a90aeccd8df62dd193902a691275ba4288f9
Nathan Bataille <nathan.bataille02@gmail.com>
2021-08-09 19:45:32 +0200

    profile creation

---
+++ controllers/profilesController.js
@@ -0,0 +1,41 @@
+let connection = require('../db.js');
+let Profile = require('../models/profilesModel.js');
+listeProfiles = [];
+
+//add a profile to the list 
+exports.addProfile = function (request, response) {
+    let profile = new Profile(req.body.id, req.body.firstname, req.body.lastname);
+    connection.query("INSERT INTO profiles set ?", profile, function (error, resultSQL) {
+        if (error) {
+            res.status(400).send(error);
+        } else {
+            res.status(201).redirect('/homepage');
+        }
+    });
+};
+
+//delete a profile from the list 
+exports.deleteProfile = function (request, response) {
+    connection.query("DELETE FROM 'profiles' WHERE 'profiles.'id' = ?", [req.params.id], (error, resultSQL) => {
+        if (error) {
+            res.status(400).send(error);
+        } else {
+            res.redirect('/homepage');
+
+        }
+    });
+};
+
+//update a profile from the list 
+exports.updateProfile = function (request, response) {
+    let profile = new Profile(req.body.id, req.body.firstname, req.body.lastname);
+    connection.query("UPDATE profiles SET ? WHERE id = ?",
+    
+        [profile, req.body.id], function (error, resultSQL) {
+            if (error) {
+                res.status(400).send(error);
+            } else {
+                res.status(202).redirect('/homepage');
+        }
+    })
+};
---
+++ models/profilesModel.js
@@ -0,0 +1,10 @@
+class Profile {
+    constructor (id, firstname, lastname)
+    {
+        this.id = id;
+        this.firstname = firstname;
+        this.lastname = lastname;
+    }
+}
+
+module.exports = Profile;
