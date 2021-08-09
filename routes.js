4a5516a1460a7f9e79112cc46c23b6260acecd43
Nathan Bataille <nathan.bataille02@gmail.com>
2021-08-09 23:59:10 +0200

    suite routes

--- routes.js
+++ routes.js
@@ -4,14 +4,30 @@ let router = express.Router();
 //***********************MVC routes***********************//
 let homepageController = require('./controllers/homepageController.js');
 let instrumentsController = require('./controllers/instrumentsController.js');
+let profilesController = require('./controllers/profilesController.js');
 //let artistController = require('./controllers/artistController.js');
 //let settingsArtistController = require('./controllers/settingsArtistController.js')
 
-//les routes 
+//routes for the main pages
 router.get('/', function (request, response) {
     response.redirect ('/homepage');
 });
-
+//route for the homepage
 router.get('/homepage', homepageController.homepage);
+//route for the profile page 
+router.get('/createProfile', profilesController.createProfile);
+
+
+
+//MVC profiles actions
+//add a profile
+router.post('/createProfile/add/', profilesController.addProfile);
+// Supprime un élément de la liste des joueurs
+router.get('/createProfile/delete/:id', profilesController.deleteProfile);
+// Modifier un élément de la liste joueurs 
+router.get('/createProfile/update/:id', profilesController.updateProfile);
+
+
+
 
 module.exports = router; 
