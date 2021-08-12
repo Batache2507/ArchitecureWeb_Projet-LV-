12d3c5d700a99a77a91f65f6f74e9753ba0454ce
Nathan Bataille <nathan.bataille02@gmail.com>
2021-08-12 12:06:27 +0200

    my profile connexion

--- controllers/profilesController.js
+++ controllers/profilesController.js
@@ -19,37 +19,40 @@ exports.feed = function (request, response) {
 }
 
 exports.signIn = function (request, response) {
-    let data;
-    connection.query("SELECT * FROM users WHERE nickname = ?;", request.body.nickname, function (error, resultSQL) {
+    //let data;
+    connection.query("SELECT * FROM `users` WHERE `nickname` = ` ? ` ", request.body.nickname, function (error, resultSQL) {
         if (error) {
+            response.status(400).send(error);
             //console.log(error);
-            return;
         } else {
-            data = resultSQL
-        }
-    });
-    console.log(data)
-    console.log("WTF")
-    signInSuite(response, data)
-    return;
-}
+            //data = resultSQL
+            connection.query("SELECT * FROM `songs` WHERE `songs`.`user_id` = ` ? ` ", function (error, resultSQL) {
+                if (error) {
+                    response.status(400).send(error);
+                }
+            });
+        };
 
-function signInSuite(response, result){
-    connection.query("SELECT * FROM `songs` WHERE `songs`.`user_id` = 1;"), function (error, resultSQL) {
-        if (error) {
-            
-            return;
-            //console.log(error);
-        }
-        else {
-            response.render('profile.ejs', { user: result, songs: resultSQL });
-        }
-    }
-}
+    })
+};
+
+//function signInSuite(response, result) {
+// console.log("WTF2")
+// connection.query("SELECT * FROM songs WHERE songs.user_id = ?;", function (error, resultSQL) {
+//  if (error) {
+//   response.status(400).send(error);
+//return;
+//console.log(error);
+//  }
+//   else {
+//       response.render('profile.ejs', { user: result, songs: resultSQL });
+//   }
+//  });
+//}
 
 
 
-//add a profile to the list 
+//add a profile to the list of profiles (for the feed)
 exports.addProfile = function (request, response) {
     let profile = new User(request.body.id, request.body.firstname, request.body.lastname, request.body.nickname, request.body.region, request.body.musical_genre, request.body.instrument);
     connection.query("INSERT INTO users set ?", profile, function (error, resultSQL) {
@@ -61,7 +64,7 @@ exports.addProfile = function (request, response) {
     });
 };
 
-//delete a profile from the list 
+//delete a profile from the list of profiles (of the feed) and his songs linked
 exports.deleteProfile = function (request, response) {
     connection.query("DELETE FROM 'users' WHERE 'users.'id' = ?", [request.params.id], (error, resultSQL) => {
         if (error) {
@@ -79,7 +82,7 @@ exports.deleteProfile = function (request, response) {
 //add a song to profile 
 exports.addSong = function (request, response) {
     let song = new Song(request.body.id, request.body.title, request.body.duration, request.body.user_id)
-    connection.query("INSERT INTO songs SET ?",song, (error, resultSQL) => {
+    connection.query("INSERT INTO songs SET ?", song, (error, resultSQL) => {
         if (error) {
             response.status(400).send(error);
         } else {
